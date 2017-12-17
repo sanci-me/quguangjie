62a5220ef65df0e53f06e2c60bd7b0c2116ced9a
sanci-me <543528249@qq.com>
2017-12-17 17:42:56 +0800

    添加微博头条文章解析

--- src/weibo-parser.js
+++ src/weibo-parser.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const puppeteer = require('puppeteer');
+const weiboTTArticleParser = require('./weibo-ttarticle-parser')
 
 async function parse(url) {
     const browser = await puppeteer.launch({
@@ -28,6 +29,16 @@ async function parse(url) {
             imgs = imgList.map(img => 'https:' + decodeURIComponent(img))
         } catch (e) {
         }
+        // weibo toutiao wenzhang 解析
+        let ttArticleUrl = ''
+        try {
+            const ttArticle = document.querySelectorAll('.WB_media_wrap div[action-type="widget_articleLayer"]')[0]
+            if (ttArticle) {
+                const ttData = ttArticle.getAttribute('action-data').split('&').find(item => item.indexOf('url') === 0)
+                ttArticleUrl = 'https:' + decodeURIComponent(ttData.split('=')[1])
+            }
+        } finally {}
+
         let videoSrc = ''
         try {
             const videoActionData = document.querySelectorAll('.WB_feed_detail li.WB_video')[0].getAttribute('video-sources')
@@ -45,16 +56,23 @@ async function parse(url) {
             content: '<p>' + contentStr.replace(/<a\b[^>]*>(.*?)<\/a>/gi,"").replace(/^(\n)?\s+/g, '') + '</p>',
             imgs: imgs,
             video: videoSrc,
-            videopic: videopic
+            videopic: videopic,
+            ttArticleUrl: ttArticleUrl
         }
     });
-
-    const imgStr = content.imgs.map(i => '<p><img src="' + i +'"/></p>').join('')
-
     browser.close()
+    const imgStr = content.imgs.map(i => '<p><img src="' + i +'"/></p>').join('')
+    let detailContent = content.content + imgStr
+    const ttArticleUrl = content.ttArticleUrl
+    if (ttArticleUrl) {
+        console.log(ttArticleUrl)
+        detailContent = await weiboTTArticleParser(ttArticleUrl)
+        return detailContent
+    }
+
     return {
         title: content.title,
-        content: content.content + imgStr,
+        content: detailContent,
         video: content.video,
         videopic: content.videopic
     }
